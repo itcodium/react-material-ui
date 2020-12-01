90529119c2beffd698cc56ca7bd56ec943220d94
pablo <itcodium>
2020-11-30 22:23:14 -0300

    api call redux

--- src/modules/ReactCourse/ReduxComplete/RecipeBook/Api.js
+++ src/modules/ReactCourse/ReduxComplete/RecipeBook/Api.js
@@ -1,16 +1,12 @@
 
-const BASE_URL = "";
-
 const apiMiddleware = ({ dispatch }) => next => action => {
     if (action.type !== 'API') {
-        console.log('action: ', action);
         return next(action);
     }
     const { payload } = action;
-    console.log('*** payload: ', payload);
     const handleError = error => dispatch({ type: payload.error, payload: error });
     dispatch({ type: payload.pending });
-    fetch(BASE_URL + payload.url)
+    fetch(payload.url)
         .then(response => {
             if (response.status >= 300) {
                 handleError(response.status);
